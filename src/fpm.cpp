2943138d6e29e569a37f627832ba802dd250da64
Markevych Alexander <rabota.pmr@gmail.com>
2016-09-08 19:13:36 +0300

    Replace string_format from using unique_ptr

--- src/fpm.cpp
+++ src/fpm.cpp
@@ -79,14 +79,36 @@ size_t fpm::write_data(void *ptr, size_t size, size_t nmemb, std::stringstream *
 	return n;
 }
 
+std::string fpm::string_format(const std::string fmt, ...) {
+    int size = ((int)fmt.size()) * 2 + 50;   // Use a rubric appropriate for your code
+    std::string str;
+    va_list ap;
+    while (1) {     // Maximum two passes on a POSIX system...
+        str.resize(size);
+        va_start(ap, fmt);
+        int n = vsnprintf((char *)str.data(), size, fmt.c_str(), ap);
+        va_end(ap);
+        if (n > -1 && n < size) {  // Everything worked
+            str.resize(n);
+            return str;
+        }
+        if (n > -1)  // Needed size returned
+            size = n + 1;   // For null char
+        else
+            size *= 2;      // Guess at a larger size (OS specific)
+    }
+    return str;
+}
+
+/*
 std::string fpm::string_format(const std::string fmt_str, ...)
 {
-	int final_n, n = ((int)fmt_str.size()) * 2; /* Reserve two times as much as the length of the fmt_str */
+	int final_n, n = ((int)fmt_str.size()) * 2; // Reserve two times as much as the length of the fmt_str 
 	std::string str;
-	std::unique_ptr<char[]> formatted;
+	std::auto_ptr<char[]> formatted;
 	va_list ap;
 	while(1) {
-		formatted.reset(new char[n]); /* Wrap the plain char array into the unique_ptr */
+		formatted.reset(new char[n]); // Wrap the plain char array into the unique_ptr 
 		strcpy(&formatted[0], fmt_str.c_str());
 		va_start(ap, fmt_str);
 		final_n = vsnprintf(&formatted[0], n, fmt_str.c_str(), ap);
@@ -99,6 +121,7 @@ std::string fpm::string_format(const std::string fmt_str, ...)
 	}
 	return std::string(formatted.get());
 }
+*/
 
 process::process(const pugi::xml_node &xml)
 {
